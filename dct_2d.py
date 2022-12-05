a58bbd540a2c99ea397ebaf21433966ac511d7f1
Ahmed Adel <69484554+Ahmed23Adel@users.noreply.github.com>
2022-12-05 19:12:32 +0200

    implmenting DCT and IDCT
    They might get updated from another device

---
+++ dct_2d.py
@@ -0,0 +1,99 @@
+import numpy as np
+import math
+import copy
+
+
+def quantization(x):
+    if x == 1:
+        return [[1, 1, 1, 1, 1, 2, 2, 4],
+                 [1, 1, 1, 1, 1, 2, 2, 4],
+                 [1, 1, 1, 1, 2, 2, 2, 4],
+                 [1, 1, 1, 1, 2, 2, 4, 8],
+                 [1, 1, 2, 2, 2, 2, 4, 8],
+                 [2, 2, 2, 2, 2, 4, 8, 8],
+                 [2, 2, 2, 4, 4, 8, 8, 16],
+                 [4, 4, 4, 4, 8, 8, 16, 16]]
+    elif x == 2:
+        return [[1, 2, 4, 8, 16, 32, 64, 128],
+                 [2, 4, 4, 8, 16, 32, 64, 128],
+                 [4, 4, 8, 16, 32, 64, 128, 128],
+                 [8, 8, 16, 32, 64, 128, 128, 256],
+                 [16, 16, 32, 64, 128, 128, 256, 256],
+                 [32, 32, 64, 128, 128, 256, 256, 256],
+                 [64, 64, 128, 128, 256, 256, 256, 256],
+                 [128, 128, 128, 256, 256, 256, 256, 256]]
+
+
+def single_block_DCT_(block):
+    """
+
+    :param x_in: should be 8x8 matrix
+    :return: dct of x_in
+    """
+    assert block.shape == (8,8)
+    output = np.zeros((8, 8))
+    for u in range(8):
+        for v in range(8):
+            basis = np.zeros((8, 8))
+            for x in range(8):
+                for y in range(8):
+                    basis[x, y] = np.cos((1 / 16) * (2 * x + 1) * u * math.pi) * np.cos((1 / 16) * (2 * y + 1) * v * math.pi)
+            output[u, v] = sum(sum(block * basis))
+    return output
+
+
+
+
+def normalize(dct_output):
+    """
+    :param dct_output: output of DCT that need to be normalized
+    :return: normalized matrix
+    """
+    dct_output_new =  copy.deepcopy(dct_output)
+    dct_output_new[0,:] /= 2
+    dct_output_new[:,0] /= 2
+    dct_output_new /= 16
+    return dct_output_new
+
+
+def single_block_DCT(x_in):
+    dct_output1 = single_block_DCT_(x_in)
+    norm = normalize(dct_output1)
+    output = norm / quantization(1)
+    return output
+
+def single_block_IDCT(block):
+    output = np.zeros((8, 8))
+    for u in range(8):
+        for v in range(8):
+            basis = np.zeros((8, 8))
+            for x in range(8):
+                for y in range(8):
+                    basis[x, y] = np.cos((1 / 16) * (2 * x + 1) * u * math.pi) * np.cos((1 / 16) * (2 * y + 1) * v * math.pi)
+            output = output + block[u, v] * basis
+    output = np.multiply(output, quantization(1))
+    return output
+
+def dct_2d(input):
+    output = np.zeros(input.shape)
+    for anchor_row in range(0, input.shape[0], 8):
+        for anchor_col in range(0, input.shape[1], 8):
+            sub_matrix = input[anchor_row:anchor_row + 8, anchor_col:anchor_col + 8]
+            output[anchor_row:anchor_row + 8, anchor_col:anchor_col + 8] = single_block_DCT(sub_matrix)
+    return output
+
+def idct_2d(input):
+    output = np.zeros(input.shape)
+    for anchor_row in range(0, input.shape[0], 8):
+        for anchor_col in range(0, input.shape[1], 8):
+            sub_matrix = input[anchor_row:anchor_row + 8, anchor_col:anchor_col + 8]
+            output[anchor_row:anchor_row + 8, anchor_col:anchor_col + 8] = single_block_IDCT(sub_matrix)
+    return output
+
+
+
+
+
+
+
+
--- encoder_2d.py
+++ encoder_2d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import math
+from dct_2d import *
 def print_meta_data(path):
     vid_cap = cv2.VideoCapture(path)
     fps = vid_cap.get(cv2.CAP_PROP_FPS)  # OpenCV v2.x used "CV_CAP_PROP_FPS"
@@ -28,7 +29,21 @@ def read_video(path):
         success, image = vid_cap.read()
 
 
-
+def get_best_motion_from(prev_frame, current_macro_block,block_size, anchor_row, anchor_col,search_x, search_y):
+    least_mse = float('inf')
+    motion_vector = [0, 0]  # move only anchor
+    for s_row in range(max(0, anchor_row - search_y),
+                       min(prev_frame.shape[0], anchor_row + block_size[0] + search_y)):  # on rows
+        for s_col in range(max(0, anchor_col - search_x),
+                           min(prev_frame.shape[1], anchor_col + block_size[1] + search_y)):  # on columns
+            prev_frame_block = prev_frame[s_row:s_row + block_size[0], s_col:s_col + block_size[1]]
+            if s_row + block_size[0] > prev_frame.shape[0] or s_col + block_size[1] > prev_frame.shape[1]:  # if it hits the end
+                continue
+            mse = ((current_macro_block - prev_frame_block) ** 2).mean(axis=None)
+            if mse < least_mse:
+                least_mse = mse
+                motion_vector[0], motion_vector[1] = s_row - anchor_row, s_col - anchor_col
+    return tuple(motion_vector)
 
 def motion_estimation(prev_frame, current_frame,block_size, search_x = 16, search_y=16):
     """
@@ -45,19 +60,8 @@ def motion_estimation(prev_frame, current_frame,block_size, search_x = 16, searc
         for anchor_col in range(0,prev_frame.shape[1], block_size[1]):# loop on all cols
             current_macro_block = current_frame[anchor_row:anchor_row+block_size[0], anchor_col:anchor_col+block_size[1]]
             # search_x and search_y
-            least_mse = float('inf')
-            motion_vector = [0,0] # move only anchor
-            for s_row in range(max(0,anchor_row-search_y), min(current_frame.shape[0],anchor_row+block_size[0]+search_y) ): # on rows
-                for s_col in range(max(0,anchor_col-search_x), min(current_frame.shape[1],anchor_col+block_size[1]+search_x)): # on columns
-                    prev_frame_block  = prev_frame[s_row:s_row+block_size[0], s_col:s_col+block_size[1]]
-                    if s_row+block_size[0] > current_frame.shape[0] or s_col+block_size[1] > current_frame.shape[1]: # if it hits the end
-                        continue
-                    mse = ((current_macro_block - prev_frame_block) ** 2).mean(axis=None)
-                    if mse < least_mse:
-                        least_mse = mse
-                        motion_vector[0], motion_vector[1] =s_row - anchor_row ,   s_col - anchor_col
-            all_motion_estimation[idx, 0] = motion_vector[0]
-            all_motion_estimation[idx, 1] = motion_vector[1]
+            all_motion_estimation[idx, 0], all_motion_estimation[idx, 1] = \
+                get_best_motion_from(prev_frame, current_macro_block,block_size, anchor_row, anchor_col,search_x, search_y)
             idx = idx + 1
     return all_motion_estimation
 
@@ -96,6 +100,10 @@ def encode_H264(path, test = True):
     prev_frame = np.zeros(shape_img)
     for current_frame in  read_video(path):
         all_motion_estimations = motion_estimation(current_frame,prev_frame,block_size)
+        predicted_frame  = motion_compensation(prev_frame,all_motion_estimations, block_size)
+        diff = current_frame - predicted_frame
+        diff_dct = dct_2d(diff)
+        diff_idct = idct_2d(diff_dct)
         prev_frame = current_frame
         break
 
@@ -104,14 +112,43 @@ def encode_H264(path, test = True):
 def test_simple(path):
     c_f = np.array([[1] * 16 + [2] * 16] * 16 + [[3] * 16 + [4] * 16] * 16)
     p_f = np.array([[2] * 16 + [1] * 16] * 16 + [[4] * 16 + [3] * 16] * 16)
-    c_f[5, 5] = 10
-    c_f[18, 5] = 20
-    c_f[17, 28] = 20
-    c_f[28, 28] = 20
-    all_motion_estimations = motion_estimation(c_f, p_f, (16, 16))
-    for v in all_motion_estimations:
-        print(v)
-    p = motion_compensation(p_f, all_motion_estimations, (16, 16))
-    print(p)
-    for x in p:
-        print(x)
+    # c_f[5, 5] = 10
+    # c_f[18, 5] = 20
+    # c_f[17, 28] = 20
+    # c_f[28, 28] = 20
+    # all_motion_estimations = motion_estimation(c_f, p_f, (16, 16))
+    # for v in all_motion_estimations:
+    #     print(v)
+    # p = motion_compensation(p_f, all_motion_estimations, (16, 16))
+    # print(p)
+    # for x in p:
+    #     print(x)
+
+
+    # img = cv2.imread(r"D:\Zewail\Year 4\info\project\H.264\H.264-encoder-decoder\camera_man.png", 0)
+    # print(img)
+    # #cv2.imshow("orig", img)
+    # print("Starting dct")
+    # d1 = dct_2d(img)
+    # # print(d1)
+    # print("done dct")
+    # d12 = idct_2d(d1)
+    # print(d12.shape)
+    # print(d12)
+    # print("Statting IDCT")
+    # #cv2.imshow("after IDCT", d12)
+    # print("Done idct")
+    # cv2.imwrite("origianl.jpeg",img)
+    # cv2.imwrite("after_idct.jpeg",d12)("done dct")
+
+
+    for current_frame in read_video(path):
+        d1 = dct_2d(current_frame)
+        #print(d1)
+        print("done dct")
+        d12 = idct_2d(d1)
+        #print(d12)
+        print("done Idct")
+        break
+    cv2.imwrite("origianl1.jpeg", current_frame)
+    cv2.imwrite("After IDCT.jpeg",d12)
